2f06843fc471ddff809cf9157e7030e7072ceba1
Robert S Adams <robert.s.adams32@gmail.com>
2019-06-11 17:43:45 -0400

    Address model create, association between User and Address create, user has addressId

--- server/db/models/index.js
+++ server/db/models/index.js
@@ -1,4 +1,5 @@
 const User = require('./user')
+const Address = require('./address')
 
 /**
  * If we had any associations to make, this would be a great place to put them!
@@ -7,6 +8,8 @@ const User = require('./user')
  *    BlogPost.belongsTo(User)
  */
 
+Address.hasMany(User)
+User.belongsTo(Address)
 /**
  * We'll export all of our models here, so that any time a module needs a model,
  * we can just require it from 'db/models'
