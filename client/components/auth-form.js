308d8f8f261093eff1cb236777ac64283ed1de1d
Nikita.Girko <girkonv@gmail.com>
2019-06-11 16:42:42 -0400

    route for creating user, added fields to auth form(first name, last name)

--- client/components/auth-form.js
+++ client/components/auth-form.js
@@ -13,6 +13,16 @@ const AuthForm = props => {
     <div>
       <form onSubmit={handleSubmit} name={name}>
         <div>
+          <label htmlFor="firstName">
+            <small>First Name</small>
+          </label>
+          <input name="firstName" type="text" />
+
+          <label htmlFor="lastName">
+            <small>Last Name</small>
+          </label>
+          <input name="lastName" type="text" />
+
           <label htmlFor="email">
             <small>Email</small>
           </label>
@@ -64,7 +74,9 @@ const mapDispatch = dispatch => {
       const formName = evt.target.name
       const email = evt.target.email.value
       const password = evt.target.password.value
-      dispatch(auth(email, password, formName))
+      const firstName = evt.target.firstName.value
+      const lastName = evt.target.lastName.value
+      dispatch(auth(email, password, firstName, lastName, formName))
     }
   }
 }
