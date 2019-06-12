3838ec1a86d43e066c7d4040b1b960b3c8c418b5
Nikita.Girko <girkonv@gmail.com>
2019-06-12 11:01:19 -0400

    issue with firstname and lastname not empty

--- script/seed.js
+++ script/seed.js
@@ -8,8 +8,18 @@ async function seed() {
   console.log('db synced!')
 
   const users = await Promise.all([
-    User.create({email: 'cody@email.com', password: '123'}),
-    User.create({email: 'murphy@email.com', password: '123'})
+    User.create({
+      email: 'cody@email.com',
+      password: '123',
+      firstName: 'Cody',
+      lastName: 'thePug'
+    }),
+    User.create({
+      email: 'murphy@email.com',
+      password: '123',
+      firstName: 'Murphy',
+      lastName: 'theSnake'
+    })
   ])
 
   const products = await Promise.all([
