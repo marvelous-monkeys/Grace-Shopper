564f5076ce9cc55c7d562ba8ac8da10326d28880
Nikita.Girko <girkonv@gmail.com>
2019-06-13 11:03:17 -0400

    minor fix in routes - route --> cart

--- client/routes.js
+++ client/routes.js
@@ -21,7 +21,7 @@ class Routes extends Component {
         {/* Routes placed here are available to all visitors */}
         <Route path="/login" component={Login} />
         <Route path="/signup" component={Signup} />
-        <Cart path="/cart" component={Cart} />
+        <Route path="/cart" component={Cart} />
         {isLoggedIn && (
           <Switch>
             {/* Routes placed here are only available after logging in */}
