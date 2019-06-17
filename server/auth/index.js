7061cd047128bbd8987244829f560d18af510208
Nikita.Girko <girkonv@gmail.com>
2019-06-17 14:47:42 -0400

    removed comments

--- server/auth/index.js
+++ server/auth/index.js
@@ -35,7 +35,6 @@ router.post('/signup', async (req, res, next) => {
 router.post('/logout', (req, res) => {
   req.logout()
   req.session.destroy()
-  // res.redirect('/')
   res.sendStatus(204)
 })
 
